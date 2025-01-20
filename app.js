c54a4d711a7b21bff737e3205ddbc5f2c950b358
ankit vaish <ankitvaish405@gmail.com>
2025-01-20 17:38:31 +0530

    Initial commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const passport = require("passport");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
-const MONGO_URL = ("mongodb+srv://Ankit:7290921543@wanderlust.akqq6.mongodb.net/?retryWrites=true&w=majority&appName=WanderLust");
+const MONGO_URL = ("mongodb://127.0.0.1:27017/wanderlust");
 
 main()
     .then(() => {
